4624baeb048efa483633b7bd91d5a6f2bf54d8d6
Jordan <jordansampson23@gmail.com>
2023-02-05 11:33:12 -0600

    initialize projects

---
+++ src/javascript/projects.js
@@ -0,0 +1,75 @@
+
+
+const projects = (() => {
+    let projectMap;
+
+    // Check local storage and efault project
+    if (localStorage.getItem('projects') === null) {
+        projectMap = new Map()
+        projectMap.set('My Project', {
+            title: 'My Project',
+            tasks: [
+                {
+                    title: 'Mow the yard',
+                    note: 'Rain forcasted Thursday morning',
+                    dueDate: '2023-02-10',
+                    priority: 'normal',
+                    completed: false
+                }
+            ]
+        });
+        projectMap.set('Work', {
+            title: 'Work',
+            tasks: [
+                {
+                    title: 'Get that report to Johnsons desk',
+                    note: 'Q3 expense report',
+                    dueDate: '2023-02-20',
+                    priority: 'high',
+                    completed: false
+                }
+            ]
+        })
+    } else {
+        projectMap = new Map(JSON.parse(localStorage.projectMap));
+    };
+
+    localStorage.projectMap = JSON.stringify(Array.from(projectMap));
+
+    console.log(projectMap)
+
+    
+
+    const Project = title => {
+        this.title = title
+        const tasks = []
+    }
+
+    function addProject(title) {
+        const project = Project(title)
+        if (!(projectMap.has(`${title}`))) {
+            projectMap.set(`${title}`, project)
+        } else return
+        //Display projects
+    }
+
+    function editProject() {
+        // edit code here
+        //Display projects
+    }
+
+    function deleteProject(title) {
+        if (projectMap.has(`${title}`)) {
+            projectMap.delete(`${title}`)
+        }
+        //Display projects
+    }
+
+    return {
+        projectMap,
+        addProject,
+        deleteProject
+    }
+})();
+
+export default projects;
