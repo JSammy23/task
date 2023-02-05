e6e4d9ff35415c7c973b741fa73aff2edd7b758d
Jordan <jordansampson23@gmail.com>
2023-02-05 12:10:03 -0600

    Initiialize tasks

--- src/javascript/task.js
+++ src/javascript/task.js
@@ -1,4 +1,4 @@
-
+import projects from "./projects";
 
 const tasks = (() => {
     const Task = (taskDetails) => {
@@ -8,20 +8,35 @@ const tasks = (() => {
         let completed = false;
         let priority = taskDetails.priority;
         let project;
-    
-        return {
-            taskName,
-            note,
-            dueDate,
-            completed,
-            priority,
-            project
-        }
+        let taskIndex
     };
 
+    function addTask(taskDetails) {
+        const task = Task(taskDetails)
 
+        // TODO: Push task into correct project
+        const project = projects.projectMap.get(`${taskDetails.project}`)
+        project.tasks.push(task)
 
+        // Refresh tasks in project array for DOM display
+    }
 
+    function editTask(taskDetails) {
+        const project = projects.projectMap.get(`${taskDetails.project}`)
+        project.tasks[taskIndex].title = taskDetails.title;
+        project.tasks[taskIndex].note = taskDetails.note;
+        project.tasks[taskIndex].dueDate = taskDetails.dueDate;
+        project.tasks[taskIndex].priority = taskDetails.priority;
+        // Refresh tasks for DOM display
+    }
+
+    function deleteTask(taskDetails) {
+        const project = projects.projectMap.get(`${taskDetails.project}`);
+        project.tasks.splice(taskDetails.taskIndex, 1)
+        // Refresh tasks for DOM display
+    }
+
+    //TODO function toggleTaskCompletion
 
 
 
