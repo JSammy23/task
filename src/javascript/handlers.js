7b7002c265e9973f02d05893722d7c762aa86ad8
Jordan <jordansampson23@gmail.com>
2023-02-07 12:05:40 -0600

    initialize handlers & listenForClicks fn

--- src/javascript/handlers.js
+++ src/javascript/handlers.js
@@ -1,12 +1,33 @@
-
+import dom from "./dom";
 
 const handlers = (() => {
+    function listenForClicks() {
+        let projectIndex;
+        let taskIndex;
+
+        document.addEventListener('click', (event) => {
+            const { target } = event;
+            const modalMainTitle = document.querySelector('.modal-main-title');
+            const selectedLink = document.querySelector('.selected-link');
+            const linkIndex = parseInt(target.getAttribute('data-link-index'), 10);
+
+            // Style selected link
+            if (target.classList.contains('select')) {
+                dom.selectLink(target, linkIndex)
+            }
+
+        });
+
+    };
 
 
 
 
 
-
-
+    return {
+        listenForClicks
+    }
 
 })();
+
+export default handlers;
